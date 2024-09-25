ba634a4f2fad011b8cb29f7fcef576dc833def54
ju78832 <junedgreat89@gmail.com>
2024-09-25 11:23:36 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cookieparser = require("cookie-parser");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieparser());
+
+app.get("/", (req, res) => {
+  res.send("Hi! I m Juned");
+});
+
+app.listen(3000, () => {
+  console.log("Port running at 3000");
+});
---
+++ prisma/index.js
@@ -0,0 +1,3 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = PrismaClient();
+module.exports = prisma;

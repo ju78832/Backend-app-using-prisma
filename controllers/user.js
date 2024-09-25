e2419a3cf26b691457921cd0c2cec94bb6d46020
ju78832 <junedgreat89@gmail.com>
2024-09-25 12:05:59 +0530

    Create user login

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,18 @@
 const prisma = require("../prisma/index");
+const cookietoken = require("../utils/cookietoken");
 
-export const signup = async (req, res) => {
-  const { name, email, password } = req.body;
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password,
-    },
-  });
+export const signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    cookietoken(user, res);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
